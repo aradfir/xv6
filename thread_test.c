c42bbda1d27d29c3ed28b56feefaab7b7418eda3
aradfir <aradfir@gmail.com>
2021-12-24 22:39:33 +0330

    Improved test to have wait

--- thread_test.c
+++ thread_test.c
@@ -6,17 +6,23 @@
 #include "stat.h"
 #include "user.h"
 int x=0;
-void count_up(void){
-    while(1){
+void count_up(){
+    int i;
+    for(i=0;i<10;i++){
         x++;
-        sleep(1000);
+        sleep(100);
     }
+    printf(1,"Thread function: DONE\n");
 }
 int main(void){
-    thread_creator(count_up);
-    while (1){
-        printf(1,"Value of x:%d",x);
-        sleep(1000);
+    thread_creator(&count_up,0);
+    int i=0;
+    for(i=0;i<5;i++){
+        printf(1,"Value of x:%d\n",x);
+        sleep(100);
     }
+    printf(1,"Waiting on thread to finish\n");
+    thread_wait();
+    printf(1,"Thread is done.\n");
     exit();
 }
